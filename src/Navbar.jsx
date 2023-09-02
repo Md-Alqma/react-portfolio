d10c63b2ccf54727f5a57f70bec6a9ad61f60c3e
Md-Alqma <mdalqma7@gmail.com>
2023-09-02 19:34:16 +0530

    crreated navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+
+export default function Navbar() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <MDBNavbar fixed="top" expand="lg" light bgColor="light">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="#">Alqma.Dev</MDBNavbarBrand>
+        <MDBNavbarToggler
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setShowBasic(!showBasic)}>
+          {" "}
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+      </MDBContainer>
+      <MDBCollapse navbar show={showBasic}>
+        <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
+          <MDBNavbarItem>
+            <MDBNavbarLink href="#">Home</MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="#">About</MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="#">Skills</MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="#">Portfolio</MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="#">Contact</MDBNavbarLink>
+          </MDBNavbarItem>
+        </MDBNavbarNav>
+      </MDBCollapse>
+    </MDBNavbar>
+  );
+}
