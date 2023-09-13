18a98e90913dc406eedfa34bcfadb6e197c7753d
Md-Alqma <mdalqma7@gmail.com>
2023-09-13 21:00:31 +0530

    masking background in home page

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import bg from "../../public/images/home-background.jpg";
-
+import styles from "./Section.module.css";
 const HomeBackgroundStyle = {
   backgroundImage: `url(${bg})`,
   backgroundSize: "cover",
@@ -9,10 +9,15 @@ const HomeBackgroundStyle = {
 const Home = ({ sectionStyle }) => {
   const homeText = "< Alqma.Dev />";
   return (
-    <div
-      style={{ ...sectionStyle, ...HomeBackgroundStyle }}
-      className="section 1">
-      <h1 style={{ fontSize: "5rem", color: "yellow" }}>{homeText}</h1>
+    <div className="section 1" style={sectionStyle}>
+      <div className="bg-image">
+        <img src={bg} className="img-fluid" alt="Sample" />
+        <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <p className="text-white mb-0">Can you see me?</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
